a42045b6ce253ca61d877c771c43327301c36351
zhousont <zt@heisentz.com>
2018-08-10 16:54:16 +0800

    zt: this is ok

--- result.py
+++ result.py
@@ -58,11 +58,11 @@ while 1:
         iftrade = 1
     elif nowtime[3] == 14 and nowtime[4] > 57:
         iftrade = 0
-    # if iftrade == 0:
-    #     print "Non Trading time, waiting ..."
-    #     print time.strftime("%Y%m%d %H:%M:%S", time.localtime())
-    #     time.sleep(30)
-    #     continue  # 如果非交易时间测试下单程序，需要#掉这个continue语句。
+    if iftrade == 0:
+        print "Non Trading time, waiting ..."
+        print time.strftime("%Y%m%d %H:%M:%S", time.localtime())
+        time.sleep(30)
+        continue  # 如果非交易时间测试下单程序，需要#掉这个continue语句。
 
     Stock_buy = read_csv("./result_send.csv", dtype={'stockname': str})
     # 从csv文件读取建仓目标
